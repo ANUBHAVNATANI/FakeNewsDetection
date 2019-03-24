dad89a82c6882043e445ef5fa4e7f75d2f50e1df
ANUBHAVNATANI <anubhavnatani99@gmail.com>
2019-03-24 12:16:55 +0530

    model manipulation files add

---
+++ model_protos/Model/model_Tx.py
@@ -0,0 +1,11 @@
+import numpy as np
+import re
+import lightgbm as lgb
+import data_functions as df
+
+class ToxicModel:
+    def __init__(self,purpose,modelName):
+        self.purpose = purpose
+        self.modelName = modelName
+
+    def get_Model()  
