9e5fdd27440d818e17c41d503c9b14172013f92d
ANUBHAVNATANI <anubhavnatani99@gmail.com>
2019-03-28 03:08:29 +0530

    inital create comment work

--- src/components/createComment.jsx
+++ src/components/createComment.jsx
@@ -1,6 +1,6 @@
 /*
 import React, { Component } from "react";
-import * as toxicity from '@tensorflow-models/toxicity'; 
+ 
 import { Form, TextField,TextareaField, SubmitField } from 'react-components-form';
 import Schema from 'form-schema-validation';
 //import {reactLocalStorage} from 'reactjs-localstorage';
@@ -117,19 +117,105 @@ export default CommentForm;
 */
 
 import React, { Component } from "react";
+import * as toxicity from "@tensorflow-models/toxicity";
 
 class CreateComment extends Component {
-  state = {};
+  state = {
+    comment: "",
+    label: [],
+    model: null,
+    commentToBeChecked: ""
+  };
+
+  async predict() {
+    let net = await this.state.model;
+    let comment = this.state.commentToBeChecked;
+    const result = await net.classify([comment]);
+    return result;
+  }
+  componentDidMount() {
+    //console.log("got here");
+    //load the tfjs model here
+    const threshold = 0.9;
+    const labelsToInclude = ["identity_attack", "insult", "threat", "toxicity"];
+    //loading the model
+
+    let model = toxicity.load(threshold, labelsToInclude);
+    //console.log("Successfully loaded the model");
+    //const result = model.classify(["You sick"]);
+    //console.log(result);
+
+    this.setState({
+      model: model
+    });
+  }
+
+  componentDidUpdate() {
+    //updation after the set state method
+    //console.log(this.state);
+
+    const model = this.state.model;
+    const comment = this.state.commentToBeChecked;
+    //console.log(model);
+    //console.log(comment);
+
+    if (model !== null && comment !== "") {
+      this.predict().then(result => {
+        let n = 0;
+        let c = [];
+        while (n < 4) {
+          if (result[n].results[0].match === true) {
+            c.push(result[n].label);
+          }
+          n = n + 1;
+        }
+        console.log(c);
+      });
+
+      //const data = [comment];
+      //const result = model.classify(data);
+      //console.log(result);
+    }
+  }
+
+  //helper function
+  /*
+  onCommentEnter(event) {
+    console.log(event.target.value);
+  }
+  */
+  /*
+  onCommentClick(event) {
+    console.log("Comment clicked");
+  }
+  */
+  onFormSubmit = event => {
+    //console.log("Came here");
+    this.setState({ commentToBeChecked: this.state.comment });
+    event.preventDefault();
+    //console.log("hey i am invoked");
+  };
+
   render() {
     return (
       <div>
-        <form class="ui reply form">
-          <div class="field">
-            <textarea />
+        <form className="ui reply form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <textarea
+              value={this.state.comment}
+              onChange={event => {
+                this.setState({ comment: event.target.value });
+              }}
+            />
           </div>
-          <div class="ui primary submit labeled icon button">
-            <i class="icon edit" /> Add Comment
-          </div>
+
+          <button
+            className="ui primary submit labeled icon button"
+            type="submit"
+          >
+            <i className="icon edit" />
+            Submit
+          </button>
         </form>
       </div>
     );
